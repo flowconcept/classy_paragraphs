e9ddb42029021ed4941954cce2022eb170efa580
Volker Killesreiter <killesre@gmail.com>
2015-06-01 16:57:08 +0200

    Added api doc

---
+++ classy_paragraphs.api.php
@@ -0,0 +1,78 @@
+<?php
+
+/**
+ * @file
+ * Hooks provided by the Classy paragraphs module.
+ */
+
+use Drupal\Core\Entity\FieldableEntityInterface;
+use Drupal\Core\Field\FieldStorageDefinitionInterface;
+
+/**
+ * Alters the list of options to be displayed for a field.
+ *
+ * This hook can notably be used to change the label of the empty option.
+ *
+ * @param array $options
+ *   The array of options for the field, as returned by
+ *   \Drupal\Core\TypedData\OptionsProviderInterface::getSettableOptions(). An
+ *   empty option (_none) might have been added, depending on the field
+ *   properties.
+ *
+ * @param field_definition: The field definition
+ *     (\Drupal\Core\Field\FieldDefinitionInterface).
+ * @param entity: The entity object the field is attached to
+ *     (\Drupal\Core\Entity\EntityInterface).
+ *
+ * @ingroup hooks
+ * @see classy_paragraphs_allowed_values()
+ */
+function hook_classy_paragraphs_options_alter(&$options, $definition, $entity) {
+  // Check if this is the field we want to change.
+  if ($definition->getName() == 'my_classy_paragraphs_field') {
+    // Change the label of the empty option.
+    $values['_none'] = t('== Empty ==');
+  }
+}
+
+/**
+ * Provide the allowed values for a 'classy_paragraphs' field.
+ *
+ *
+ * @param \Drupal\Core\Field\FieldStorageDefinitionInterface $definition
+ *   The field storage definition.
+ * @param \Drupal\Core\Entity\FieldableEntityInterface|null $entity
+ *   (optional) The entity context if known, or NULL if the allowed values are
+ *   being collected without the context of a specific entity.
+ *
+ * @return array
+ *   The array of allowed values. Keys of the array are the raw stored values
+ *   (number or text), values of the array are the display labels. If $entity
+ *   is NULL, you should return the list of all the possible allowed values in
+ *   any context so that other code (e.g. Views filters) can support the allowed
+ *   values for all possible entities and bundles.
+ *
+ * @ingroup hooks
+ * @see classy_paragraphs_allowed_values()
+ */
+function hook_classy_paragraphs_options(FieldStorageDefinitionInterface $definition, FieldableEntityInterface $entity = NULL) {
+  if (isset($entity) && ($entity->bundle() == 'not_a_programmer')) {
+    $values = array(
+      1 => 'One',
+      2 => 'Two',
+    );
+  }
+  else {
+    $values = array(
+      'Group 1' => array(
+        0 => 'Zero',
+        1 => 'One',
+      ),
+      'Group 2' => array(
+        2 => 'Two',
+      ),
+    );
+  }
+
+  return $values;
+}
